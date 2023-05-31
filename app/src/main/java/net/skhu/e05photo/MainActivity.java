2aed080c8573a7185eabcb0372b386c23dd0129e
Kim gyeongE <binggre05@naver.com>
2023-05-31 14:33:53 +0900

    5/31 수요일

--- app/src/main/java/net/skhu/e05photo/MainActivity.java
+++ app/src/main/java/net/skhu/e05photo/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Class classObj = null;
         switch (view.getId()) {
             case R.id.button1: classObj = Photo1Activity.class; break;
+            case R.id.button2: classObj = Photo2Activity.class; break;
+            case R.id.button3: classObj = Photo3Activity.class; break;
         }
         Intent intent = new Intent(this, classObj);
         startActivity(intent);
---
+++ app/src/main/java/net/skhu/e05photo/Photo3Activity.java
@@ -0,0 +1,41 @@
+package net.skhu.e05photo;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import net.skhu.e05photo.databinding.ActivityPhoto3Binding;
+
+public class Photo3Activity extends AppCompatActivity {
+
+    private ActivityPhoto3Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityPhoto3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
